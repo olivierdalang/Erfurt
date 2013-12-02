45affd8c28a59a9aee489117f2a34b62516b71c8
Andreas Nareike <nareike@informatik.uni-leipzig.de>
2013-12-02 22:27:41 +0100

    add option to export SPARQL result to CSV
    This adds an option to the Virtuoso adapter to export queries as
    CSV. For better structuring, the CSV converter should be moved up a
    level to be called from the Erfurt/Store.php. Right now, there is not
    much of output format handling there, but it would probably be best to
    check for supported formats (as suggested by the TODO in line 1565) and
    use either a default converter or (if supported) the custom (and
    hopefully faster) method of the used store.

--- library/Erfurt/Store/Adapter/Virtuoso.php
+++ library/Erfurt/Store/Adapter/Virtuoso.php
@@ -1143,6 +1143,12 @@ class Erfurt_Store_Adapter_Virtuoso implements Erfurt_Store_Adapter_Interface, E
                 'converter'   => null,
                 'jsonEncode'  => false,
                 'queryPrefix' => ''
+            ),
+            'csv' => array(
+                'singleField' => false,
+                'converter'   => 'CSV',
+                'jsonEncode'  => false,
+                'queryPrefix' => ''
             )
         );
 
